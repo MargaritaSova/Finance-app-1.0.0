75b29130f716d75340646ec34e1aaaebb8440f6e
Margarita <sovamargaritaa@gmail.com>
2019-03-04 23:18:23 +0200

    It works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //find elements
 let number1Input = document.getElementById('money');
-let description = document.getElementById("comment");
+let description = document.getElementById('comment');
 let dateEl = document.getElementById('date');
 let income = document.getElementById('income');
 let consumption = document.getElementById('consumption');
@@ -12,8 +12,6 @@ let allConsumption = document.getElementById('all-expense');
 let balance = document.getElementById('balance');
 let deleteBtn = document.querySelector('.delete');
 
-
-// comments = [];
 
 income.addEventListener('change', onIncomeClick);
 consumption.addEventListener('change', onConsumptionClick);
@@ -28,10 +26,6 @@ number1Input.onkeypress = function (e) {
     if (e.ctrlKey || e.altKey || e.metaKey) return;
 
     var chr = getChar(e);
-
-    // ñ null íàäî îñòîðîæíî â íåðàâåíñòâàõ,
-    // ò.ê. íàïðèìåð null >= '0' => true
-    // íà âñÿêèé ñëó÷àé ëó÷øå âûíåñòè ïðîâåðêó chr == null îòäåëüíî
     if (chr == null) return;
 
     if (chr < '0' || chr > '9') {
@@ -47,10 +41,9 @@ function getChar(event) {
 
     if (event.which != 0 && event.charCode != 0) {
         if (event.which < 32) return null;
-        return String.fromCharCode(event.which) // îñòàëüíûå
+        return String.fromCharCode(event.which);
     }
-
-    return null; // ñïåöèàëüíàÿ êëàâèøà
+    return null; 
 }
 //////////////////////////////////////////////////////////////////////////////
 function onIncomeClick() {
@@ -64,53 +57,61 @@ function onConsumptionClick() {
         income.checked = false;
     }
 }
-/////////////////////////////////////////////////////////////////////////////////
-// function onAddClick() {
-
-//     let comment = {
-//         text: number1Input.value,
-//         date: dateEl.value
-//     }
-//     comments.push(comment);
-//     showComments();
-// }
-
-
-comments = [];
 ////////////////////////////////////////////////////////////////////
+commentsIncome = [];
+commentsConsumption = [];
 function onAddClick() {
     let li = document.createElement('li');
     let changeBtn = document.createElement('button'); 
     let deleteBtn = document.createElement('button');
-    let text = document.createElement('label');
+    let amount = document.createElement('label');
     let textfield = document.createElement('input');
     let comment = document.createElement('p');
     let date = document.createElement('p');
 
-    changeBtn.className += "change";
-    deleteBtn.className +="delete";
-    comment.className += "description";
-    textfield.className +="textfield";  
+    comment.className += 'description';
+    textfield.className +='textfield';  
 
-    changeBtn.innerHTML = "change";
-    deleteBtn.innerHTML = "delete";
-    text.innerHTML = number1Input.value;
+    changeBtn.innerHTML = 'Изменить';
+    deleteBtn.innerHTML = 'Удалить';
+    amount.innerHTML = number1Input.value;
     comment.innerHTML = description.value;
     date.innerHTML = dateEl.value;
 
 
     if (income.checked) {
-         li.className += "income"
-         text.className += "income-amount";
-                
-         incomeEl.insertBefore(li, incomeEl.children[1]);    
+         li.id = 'income';
+         amount.className += 'income-amount';
+		 deleteBtn.className = 'delete-income';
+		 changeBtn.className += 'change-income';
+
+         incomeEl.insertBefore(li, incomeEl.children[0]);  
+		 
+		 let comment1 = {
+			numb: number1Input.value,
+		 }
+		 commentsIncome.push(comment1);
+
+		 deleteBtn.addEventListener('click', deleteItemFromIncome);
+		 changeBtn.addEventListener('click', changeItemIncome);
 
          addAllIncome();
      } else if (consumption.checked) {
-         li.className += "consumption"
-         text.className += "consumption-amount";
+         li.className += 'consumption';
+         amount.className += 'consumption-amount';
 
-         consumptionEl.insertBefore(li, consumptionEl.children[1]);
+		 deleteBtn.className += 'delete-consumption';
+		 changeBtn.className += 'change-consumption';
+
+         consumptionEl.insertBefore(li, consumptionEl.children[0]);
+
+		 let comment1 = {
+			 numb: number1Input.value,
+		}
+		commentsConsumption.push(comment1);
+
+		deleteBtn.addEventListener('click', deleteItemFromConsumption);
+		changeBtn.addEventListener('click', changeItemConsumption);
 
          addAllConsumption();
      } else {
@@ -119,66 +120,57 @@ function onAddClick() {
 
     li.appendChild(changeBtn);
     li.appendChild(deleteBtn);
-    li.appendChild(text);
+    li.appendChild(amount);
     li.appendChild(comment);
     li.appendChild(date);
     li.appendChild(textfield);
-
-    deleteBtn.addEventListener('click', deleteItem);
-    changeBtn.addEventListener('click', changeItem);
-
-
+	
      consumption.checked = false;
      income.checked = false;
 
-     let comment1 = {
-         numb: number1Input.value,
-     }
-     comments.push(comment1);
-
-     money.value = ''
+     number1Input.value = '';
 }
-
 //////////////////////////////////////////////////////////////////////
- let sum;
  function addAllIncome() {
+	 let sum = 0;
      if (allIncome.children.length < 2) {
          let text1 = document.createElement('p');
-         text1.className += "income";
-         text1.innerHTML = number1Input.value;
-         sum = Number(text1.innerHTML);
+         text1.className += 'income';
+         text1.innerHTML = commentsIncome[commentsIncome.length - 1].numb;
          allIncome.insertBefore(text1, allIncome.children[1]);
 
      } else {
-         let newText = document.querySelector('.income');
-         sum += Number(number1Input.value);
-         newText.textContent = sum;
+         let newText = document.querySelector('.income'); 
+		 let result = allIncomeAmount();
+		 newText.textContent = result;
      }
-     // money.value = '';
      AddCountedBalance();
-
  }
 
- let sum2;
  function addAllConsumption() {
+	 sum = 0;
      if (allConsumption.children.length < 2) {
          let text2 = document.createElement('p');
-         text2.className += "consumption";
-         text2.innerHTML = number1Input.value;
-         sum2 = Number(text2.innerHTML);
+         text2.className += 'consumption';
+         text2.innerHTML = commentsConsumption[commentsConsumption.length - 1].numb;
          allConsumption.insertBefore(text2, allConsumption.children[1]);
 
      } else {
          let newText = document.querySelector('.consumption');
-         sum2 += Number(number1Input.value);
-         newText.textContent = sum2;
+		 let result = allConsumptionAmount();
+         newText.textContent = result;
     }
-     // money.value = '';
      AddCountedBalance();
  }
 
+
  function AddCountedBalance() {
      let newBalance;
+	 let result1 = allIncomeAmount();
+	 let result2 = allConsumptionAmount();
+	 
+	 let sum = result1;
+	 let sum2 = result2;
 
      if (sum == undefined) {
          sum = 0;
@@ -190,7 +182,7 @@ function onAddClick() {
 
      if (balance.children.length < 2) {
          let text3 = document.createElement('p');
-         text3.className += "balance";
+         text3.className += 'balance';
          text3.innerHTML = newBalance;
          balance.insertBefore(text3, balance.children[1]);
      } else {
@@ -198,15 +190,59 @@ function onAddClick() {
          newText.textContent = newBalance;
      }
  }
+////////////////delete Item from two Array and li////////////////////////
+var arrayElem = [];
+ function deleteItemFromIncome(e) {
+	let h = e.target;
+	
+	let arrElems =  document.querySelectorAll('.delete-income');
+	let g;
+	for (var i = 0; i < arrElems.length; i++){
+		arrayElem.unshift(arrElems[i]);
+		g = arrayElem.indexOf(e.target);
+	}
+	arrayElem.splice(g, 1);
+	commentsIncome.splice(g, 1);
 
-/////////////////////delete Item///////////
- function deleteItem() {
     const listItem = this.parentNode;
     let list = listItem.parentNode;
     list.removeChild(listItem);
+
+	newAmountInAllIncome();
+	AddCountedBalance();
+}
+
+var arrayElem2 = [];
+ function deleteItemFromConsumption(e) {
+	//let h = e.target;
+	
+	let arrElems =  document.querySelectorAll('.delete-consumption');
+	let g;
+	for (var i = 0; i < arrElems.length; i++){
+		arrayElem2.unshift(arrElems[i]);
+		g = arrayElem2.indexOf(e.target);
+	}
+	arrayElem2.splice(g, 1);
+	commentsConsumption.splice(g, 1);
+
+    const listItem = this.parentNode;
+    let list = listItem.parentNode;
+    list.removeChild(listItem);
+
+	newAmountInAllConsumption();
+	AddCountedBalance();
 }
 ////////////////change Item/////////////////
- function changeItem() {
+let arrayElem3 = [];
+ function changeItemIncome(e) {
+	let arrElems = document.querySelectorAll('.change-income');
+
+	let g;
+	for(let i = 0; i < arrElems.length; i ++){
+		arrayElem3.unshift(arrElems[i]);
+		g = arrayElem3.indexOf(e.target);
+	}
+
      const listItem = this.parentNode;
      const title = listItem.querySelector('.income-amount');
      const editInput = listItem.querySelector('.textfield');
@@ -220,18 +256,70 @@ function onAddClick() {
          this.innerText = 'Сохранить';
      }
      listItem.classList.toggle('editing');
-     return editInput;
+
+	 commentsIncome[g].numb = title.innerText;
+
+	 newAmountInAllIncome();
  }
 
-//  function changeArray(editInput){
-//     const j = editInput.value;
-//     const l = listItem.querySelector('.textfield');
-//     let h = l.value;
-//     // this.textfield = 
-//     comments.forEach(function (comment) {
-//       if (comment.numb == '1') {
-//         comment.numb = 666;
-//       }    
-// });
-// }
-// changeArray();
+ let arrayElem4 = [];
+ function changeItemConsumption(e) {
+	let arrElems = document.querySelectorAll('.change-consumption');
+
+	let g;
+	for(let i = 0; i < arrElems.length; i ++){
+		arrayElem4.unshift(arrElems[i]);
+		g = arrayElem4.indexOf(e.target);
+	}
+
+     const listItem = this.parentNode;
+     const title = listItem.querySelector('.consumption-amount');
+     const editInput = listItem.querySelector('.textfield');
+     const isEditing = listItem.classList.contains('editing');
+
+      if(isEditing) {
+          title.innerText = editInput.value;
+          this.innerText = 'Изменить';
+      } else {
+         editInput.value = title.innerText;
+         this.innerText = 'Сохранить';
+     }
+     listItem.classList.toggle('editing');
+
+	 commentsConsumption[g].numb = title.innerText;
+
+	 newAmountInAllConsumption();
+ }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+function newAmountInAllIncome() {
+	let p = document.querySelector('.income');
+	let result = allIncomeAmount();
+	p.innerHTML = result;
+	AddCountedBalance();
+}
+
+function newAmountInAllConsumption() {
+	let p = document.querySelector('.consumption');
+	let result = allConsumptionAmount();
+	p.innerHTML = result;
+	AddCountedBalance();
+}
+
+
+function allIncomeAmount() {
+	let amount = 0;
+	commentsIncome.forEach(function (comment) {
+		amount += Number(comment.numb);
+	})
+	return amount;
+}
+
+function allConsumptionAmount() {
+	let amount = 0;
+	commentsConsumption.forEach(function (comment) {
+		amount += Number(comment.numb);
+	})
+	return amount;
+}
